a791d0bea267da5b000dc251ae5695cf62c322fc
Mihai Tuinea <mihai.tuinea27@gmail.com>
2025-07-02 18:27:22 +0300

    backup

--- ShowTime/Entities/Band.cs
+++ ShowTime/Entities/Band.cs
@@ -1,14 +1,17 @@
-﻿using ShowTime.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using ShowTime.Enums;
 
 namespace ShowTime.Entities
 {
     public class Band
     {
         public Guid Id { get; set; }
-        public required string Name { get; set; }
-        public Genre Genre { get; set; }
-        public ICollection<Festival> Festivals { get; set; } = [];
 
-        public ICollection<Member> Members { get; set; } = [];
+        [Required(ErrorMessage = "Name is required.")]
+        public string? Name { get; set; }
+
+        public Genre Genre { get; set; }
+        public ICollection<Festival> Festivals { get; set; } = new List<Festival>();
+        public ICollection<Member> Members { get; set; } = new List<Member>();
     }
 }
