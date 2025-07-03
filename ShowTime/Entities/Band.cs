141f3f7e8baa1624f74cc904630c39c50f753a30
Mihai Tuinea <mihai.tuinea27@gmail.com>
2025-07-03 22:50:18 +0300

    fixed & implemented crud modals for festival

--- ShowTime/Entities/Band.cs
+++ ShowTime/Entities/Band.cs
@@ -1,10 +1,12 @@
 ﻿using ShowTime.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace ShowTime.Entities
 {
     public class Band
     {
         public Guid Id { get; set; }
+        [Required(ErrorMessage = "Band Name must not be empty")]
         public string? Name { get; set; }
         public Genre Genre { get; set; }
         public ICollection<Festival> Festivals { get; set; } = [];
--- ShowTime/Entities/Festival.cs
+++ ShowTime/Entities/Festival.cs
@@ -1,9 +1,13 @@
-﻿namespace ShowTime.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShowTime.Entities
 {
     public class Festival
     {
         public Guid Id { get; set; }
+        [Required(ErrorMessage = "Festival Name must not be empty")]
         public string? Name { get; set; }
+        [Required(ErrorMessage = "Festival Location must not be empty")]
         public string? Location { get; set; }
         public string? Description { get; set; }
         public DateTime StartDate { get; set; }
